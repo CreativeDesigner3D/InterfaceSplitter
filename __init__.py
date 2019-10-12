f311600393800e5ac5da5270242b8876ff8dc469
Andrew Peel <blenderpro3d@gmail.com>
2019-10-12 11:11:09 -0700

    Updating Add-on Name

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 bl_info = {
-    "name": "BlenderPro Interface Splitter",
+    "name": "Interface Splitter",
     "author": "Andrew Peel",
     "version": (0, 0, 1),
     "blender": (2, 80, 0),
     "location": "3D Viewport Header Interface Menu",
-    "description": "Adds a new drop down menu that allows you to quickly split the interface",
+    "description": "Adds a new drop down menu to the 3D Viewport that allows you to quickly split the interface",
     "warning": "",
     "wiki_url": "",
-    "category": "BlenderPro",
+    "category": "3D View",
 }
 
 from .ui import bp_view3d_ui_header
